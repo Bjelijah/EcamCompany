f230e3b1a100a46e05c34cd3f4e4ce2628396888
Bjelijah <elijah@live.cn>
2016-07-28 09:06:01 +0800

    e看企业版+群艺馆更新

---
+++ obj/local/armeabi/objs/player_jni/streamreq_jni.o.d
@@ -0,0 +1,157 @@
+obj/local/armeabi/objs/player_jni/streamreq_jni.o: jni/streamreq_jni.c \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/jni.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/android/api-level.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/pthread.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/time.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/time.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/stdint.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/_wchar_limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/posix_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/stddef.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/compiler.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/posix_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/kernel.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/sysmacros.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/time.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/siginfo.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm-generic/siginfo.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/signal.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/internal_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/syslimits.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/page.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/string.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/malloc.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/signal.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm-generic/signal.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/sigcontext.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/ucontext.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/user.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sched.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/android/log.h \
+ jni/include/ecamstreamreq.h jni/include/ice.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/stdio.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/stdlib.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/alloca.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/strings.h \
+ E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/memory.h \
+ jni/include/osipparser2/sdp_message.h \
+ jni/include/osipparser2/osip_list.h jni/include/ice.h \
+ jni/include/hwplay/stream_type.h jni/include/hwplay/play_def.h \
+ jni/include/hwplay/hw_config.h
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/jni.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/android/api-level.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/pthread.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/time.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/time.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/stdint.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/_wchar_limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/posix_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/stddef.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/compiler.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/posix_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/kernel.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/time.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/siginfo.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm-generic/siginfo.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/signal.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/linux/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/internal_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/machine/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/syslimits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/page.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/string.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/malloc.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/signal.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm-generic/signal.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/asm/sigcontext.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/ucontext.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/user.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sched.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/android/log.h:
+
+jni/include/ecamstreamreq.h:
+
+jni/include/ice.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/stdio.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/stdlib.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/alloca.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/strings.h:
+
+E:/Android/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/memory.h:
+
+jni/include/osipparser2/sdp_message.h:
+
+jni/include/osipparser2/osip_list.h:
+
+jni/include/ice.h:
+
+jni/include/hwplay/stream_type.h:
+
+jni/include/hwplay/play_def.h:
+
+jni/include/hwplay/hw_config.h:
---
+++ src/com/howell/utils/DialogUtils.java
@@ -0,0 +1,27 @@
+package com.howell.utils;
+
+import com.example.howell.webcamforcompanyofqyg.R;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface.OnClickListener;
+
+public class DialogUtils {
+	public static Dialog postWaitDialog(Context context){
+		final Dialog lDialog = new Dialog(context,android.R.style.Theme_Translucent_NoTitleBar);
+		lDialog.setContentView(R.layout.wait_dialog);
+		return lDialog;
+	}
+	
+	public static Dialog postDialogWithPositiveButton(Context context,String title ,String message,int icon,String btnName,OnClickListener listenter){
+		Dialog alertDialog = new AlertDialog.Builder(context).   
+	            setTitle(title).   
+	            setMessage(message).   
+	            setIcon(icon).   
+	            setPositiveButton(btnName, listenter).   
+	    create();   
+		return alertDialog;
+	}
+	
+}
